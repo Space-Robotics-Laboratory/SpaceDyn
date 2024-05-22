fbc630bb7b0dfe26de1986417d8a2f25ceb3c16b
masazumi_imai <imai.masazumi.p2@dc.tohoku.ac.jp>
2024-05-22 17:49:58 +0900

    Updated dc2qtn

--- src/matlab/spacedyn_v2r0/dc2qtn.m
+++ src/matlab/spacedyn_v2r0/dc2qtn.m
@@ -20,59 +20,47 @@
 %
 %   1998.6.11 K.FUJISHIMA
 %   1998.9.16 Rewritten by K.Yoshida
+%   2024.5.22 Updated by A.Uchida and M.Imai
 %
 
 function qtn = dc2qtn( C0 )
 
 % input check-out
-
-	if [3,3]~=size( C0 )
-		error('Cannot compute the quaternion\n');
-	end
-	check = det(C0);
-	if abs(check-1)>0.1,
-		fprintf('Warning: mat2qtn: input is not a normal matrix.\n');
-	end
+if ~all(size( C0 ) == [3, 3])
+    error('Cannot compute the quaternion\n');
+end
+check = det(C0);
+if abs(check - 1) > 0.1
+    fprintf('Warning: mat2qtn: input is not a normal matrix.\n');
+end
 
 % start
-	
-	qtn    = zeros(4,1);
+qtn    = zeros(4, 1);
 
-	qtn(4) = sqrt( 1 + C0(1,1) + C0(2,2) + C0(3,3) ) / 2;
+qtn = [ sqrt( 1 + C0(1,1) - C0(2,2) - C0(3,3) ) / 2;
+        sqrt( 1 - C0(1,1) + C0(2,2) - C0(3,3) ) / 2;
+        sqrt( 1 - C0(1,1) - C0(2,2) + C0(3,3) ) / 2;
+        sqrt( 1 + C0(1,1) + C0(2,2) + C0(3,3) ) / 2];
 
-	if qtn(4) > 0.001
+% So as not to divide by zero element
+[qtn_max, idx_qtn_max] = max(qtn);
 
-% In case qtn(4) is not small,
-
-		qtn(1:3) = [ C0(2,3)-C0(3,2);
-		             C0(3,1)-C0(1,3);
-	        	     C0(1,2)-C0(2,1) ] / (4*qtn(4));
-	else
-
-% In case qtn(4) is small,
-
-		qtn(1) = sqrt( 1 + C0(1,1) - C0(2,2) - C0(3,3) ) / 2;
-		if qtn(1) > 0.001,
-			qtn(2:4) = [ C0(1,2)+C0(2,1);
-		            	     C0(3,1)+C0(1,3);
-	        	    	     C0(3,2)+C0(2,3) ] / (4*qtn(1));
-		else
-			det(C0)
-			error('Cannot compute the quaternion. Check if the direction cosine is a normal orthogonal matrix.\n');
-		end
-
-	end
+switch idx_qtn_max
+    case 1
+      qtn([2 3 4]) = [C0(1,2) + C0(2,1); C0(1,3) + C0(3,1); C0(2,3) - C0(3,2)] / (4 * qtn_max);
+    case 2
+      qtn([1 3 4]) = [C0(1,2) + C0(2,1); C0(3,2) + C0(2,3); C0(3,1) - C0(1,3)] / (4 * qtn_max);
+    case 3
+      qtn([1 2 4]) = [C0(3,1) + C0(1,3); C0(3,2) + C0(2,3); C0(1,2) - C0(2,1)] / (4 * qtn_max);
+    case 4
+      qtn([1 2 3]) = [C0(2,3) - C0(3,2); C0(3,1) - C0(1,3); C0(1,2) - C0(2,1)] / (4 * qtn_max);
+end
 
 % precision check-out
-	
-	check = qtn'*qtn;
-	if abs(check-1)>0.1
-		fprintf('Warning: mat2qtn: precision in attitude computation is not guaranteed.\n')
-	end
+check = qtn' * qtn;
+if abs(check - 1) > 0.1
+    fprintf('Warning: mat2qtn: precision in attitude computation is not guaranteed.\n')
+end
 
+end
 %%% EOF
-
-
-
-
-
