8ae8e1271f2b81d9a0a8cd172a001c17795f24ed
masazumi_imai <imai.masazumi.p2@dc.tohoku.ac.jp>
2024-05-22 17:50:43 +0900

    Updated f_dyn_rk2

--- src/matlab/spacedyn_v2r0/f_dyn_rk2.m
+++ src/matlab/spacedyn_v2r0/f_dyn_rk2.m
@@ -26,6 +26,7 @@
 %
 %   1998.9.2  written by K.FUJISHIMA
 %   2002.2.27  H.Nakanishi modified to version up
+%   2024.5.22  Updated by A.Uchida and M.Imai
 %
 
 function SV = f_dyn_rk2(LP, SV)
@@ -34,17 +35,21 @@ global Ez Gravity d_time
 
 
 % 1st Step
-SVt = f_dyn(LP,SV);
+SV.Qtn0 = dc2qtn_2(SV.A0');
+SV.dQtn0 = w2dQtn(SV.w0, SV.Qtn0);
+SVt = f_dyn(LP, SV);
 
 k1_R0 = d_time * SV.v0;
-k1_A0 = aw( SV ) * SV.A0 - SV.A0;
+k1_Qtn0 = d_time * SV.dQtn0;
 k1_v0 = d_time * SVt.vd0;
 k1_w0 = d_time * SVt.wd0;
 k1_q  = d_time * SV.qd;
 k1_qd = d_time * SVt.qdd;
 
 SVt.R0 = SV.R0 + k1_R0/2;
-SVt.A0 = SV.A0 + k1_A0/2;
+SVt.Qtn0 = SV.Qtn0 + k1_Qtn0/2;
+SVt.Qtn0 = SVt.Qtn0 / norm(SVt.Qtn0);
+SVt.A0 = qtn2dc(SVt.Qtn0)';
 SVt.v0 = SV.v0 + k1_v0/2;
 SVt.w0 = SV.w0 + k1_w0/2;
 SVt.q  = SV.q +  k1_q/2;
@@ -52,44 +57,51 @@ SVt.qd = SV.qd + k1_qd/2;
 
 
 % 2nd Step
+SVt.dQtn0 = w2dQtn(SVt.w0, SVt.Qtn0);
 SVt = f_dyn(LP, SVt);
 
 k2_R0 = d_time * SVt.v0;
-k2_A0 = aw( SVt ) * SV.A0 - SV.A0;
+k2_Qtn0 = d_time * SVt.dQtn0;
 k2_v0 = d_time * SVt.vd0;
 k2_w0 = d_time * SVt.wd0;
 k2_q  = d_time * SVt.qd;
 k2_qd = d_time * SVt.qdd;
 
 SVt.R0 = SV.R0 + k2_R0/2;
-SVt.A0 = SV.A0 + k2_A0/2;
+SVt.Qtn0 = SV.Qtn0 + k2_Qtn0/2;
+SVt.Qtn0 = SVt.Qtn0 / norm(SVt.Qtn0);
+SVt.A0 = qtn2dc(SVt.Qtn0)';
 SVt.v0 = SV.v0 + k2_v0/2;
 SVt.w0 = SV.w0 + k2_w0/2;
 SVt.q  = SV.q  + k2_q/2;
 SVt.qd = SV.qd + k2_qd/2;
 
 % 3rd Step
+SVt.dQtn0 = w2dQtn(SVt.w0, SVt.Qtn0);
 SVt = f_dyn(LP, SVt);
 
 k3_R0 = d_time * SVt.v0;
-k3_A0 = aw( SVt ) * SV.A0 - SV.A0;
+k3_Qtn0 = d_time * SVt.dQtn0;
 k3_v0 = d_time * SVt.vd0;
 k3_w0 = d_time * SVt.wd0;
 k3_q  = d_time * SVt.qd;
 k3_qd = d_time * SVt.qdd;
 
 SVt.R0 = SV.R0 + k3_R0;
-SVt.A0 = SV.A0 + k3_A0;
+SVt.Qtn0 = SV.Qtn0 + k3_Qtn0;
+SVt.Qtn0 = SVt.Qtn0 / norm(SVt.Qtn0);
+SVt.A0 = qtn2dc(SVt.Qtn0)';
 SVt.v0 = SV.v0 + k3_v0;
 SVt.w0 = SV.w0 + k3_w0;
 SVt.q  = SV.q  + k3_q;
 SVt.qd = SV.qd + k3_qd;
 
 % 4th Step
+SVt.dQtn0 = w2dQtn(SVt.w0, SVt.Qtn0);
 SVt = f_dyn(LP, SVt);
 
 k4_R0 = d_time * SVt.v0;
-k4_A0 = aw( SVt ) * SV.A0 - SV.A0;
+k4_Qtn0 = d_time * SVt.dQtn0;
 k4_v0 = d_time * SVt.vd0;
 k4_w0 = d_time * SVt.wd0;
 k4_q  = d_time * SVt.qd;
@@ -97,7 +109,9 @@ k4_qd = d_time * SVt.qdd;
 
 % Solution
 SV.R0 = SV.R0 + ( k1_R0 + 2*k2_R0 + 2*k3_R0 + k4_R0 )/6;
-SV.A0 = SV.A0 + ( k1_A0 + 2*k2_A0 + 2*k3_A0 + k4_A0 )/6;
+SV.Qtn0 = SV.Qtn0 + ( k1_Qtn0 + 2*k2_Qtn0 + 2*k3_Qtn0 + k4_Qtn0 )/6;
+SV.Qtn0 = SV.Qtn0 / norm(SV.Qtn0);
+SV.A0 = qtn2dc(SV.Qtn0)';
 SV.v0 = SV.v0 + ( k1_v0 + 2*k2_v0 + 2*k3_v0 + k4_v0 )/6;
 SV.w0 = SV.w0 + ( k1_w0 + 2*k2_w0 + 2*k3_w0 + k4_w0 )/6;
 SV.q  = SV.q  + ( k1_q  + 2*k2_q  + 2*k3_q  + k4_q  )/6;
